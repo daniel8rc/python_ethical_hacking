0e15e9b8eacf64294a2cf09eab67764c42a418d0
daniel8rc <root@localhost.localdomain>
2019-09-16 17:53:01 -0400

    Added multi-port scanning with multithreading

--- scanner_ports.py
+++ scanner_ports.py
@@ -1,23 +1,105 @@
 #!/usr/bin/python3
 
 import socket
+import threading
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-host = "192.168.1.138"
-port = 443
 
+socket.setdefaulttimeout(2)
+
+
+class Threads:
+    def __init__(self, func, params):
+        self.func = func
+        self.params = params
+        self.threads = []
+
+    def wait_threads(self):
+        """
+        Wait for all threads to complete.
+        """
+        for t in self.threads:
+            t.join()
+        print("Finished scann")
+
+    def execute_thread(self, args):
+        
+        if type(self.params)== int:
+            thread = threading.Thread(target=self.func, args=(args,))
+        else:
+            thread = threading.Thread(target=self.func, args=args)
+
+        # Start new Threads
+        thread.start()
+        # Add threads to thread list
+        self.threads.append(thread)
+
+    def run(self):
+        self.execute_thread(self.params)
+
+
+        
 class Scanner:
-    def __init__(self, host):
-        self.host = host
+    def __init__(self):
+        self.ports = ''
+        self.port = ''
+        self.host = ''
+        self.reverse = False
+
+    def clean_multi_port(self):
+        ports = self.ports.replace(' ', '').replace('[', '').replace(']', '').split('-')
+        # To integer
+        ports = list(map(int, ports))
+
+        if len(ports) != 2:
+            raise("The structure of the ports isn't valid.")
+        self.reverse = False
+        if ports[0] > ports[1]:
+            self.reverse = True
+            ports[0], ports[1] = ports[1], ports[0]
+
+        return ports
 
     def specific_port_scanner(self, port):
-        if sock.connect_ex((host, port)):
-            print("Port %d is closed." % (port))
-        else:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        
+        if not sock.connect_ex((self.host, port)):
             print("Port %d is open." % (port))
+        # else:
+        #     print("Port %d is close." % (port))
 
+    def scann_multi_port(self):
+        if self.ports:
+            if self.reverse:
+                for p in reversed(range(self.ports[0], self.ports[1])):
+                    t = Threads(self.specific_port_scanner, p)
+                    t.run()
+                    
+            else:
+                for p in range(self.ports[0], self.ports[1]):
+                    t = Threads(self.specific_port_scanner, p)
+                    t.run()
+
+            t.wait_threads()
+                
+    def scann_ports(self):
+        self.host = str(input("[*] Enter The Host to Scan: "))
+        multiple_port = bool(
+            (input("[*] Do you want to scanning multi-port? [Yy or Nn]: ")).lower() == 'y'
+        )
+        if not multiple_port:
+            self.port = int(input("[*] Enter The Port to Scan: "))
+            self.specific_port_scanner(self.port)
+        else:
+            self.ports = str(input("[*] Enter The Ports to Scan: [InitPort-Finalport]: "))
+            self.ports = self.clean_multi_port()
+            self.scann_multi_port()
+
+
+    def run(self):
+        self.scann_ports()
 
 if __name__ == "__main__":
-    scan = Scanner(host)
-    scan.specific_port_scanner(port)
+    scan = Scanner()
+    scan.run()
+
