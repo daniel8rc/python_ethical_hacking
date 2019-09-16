229f514dfb18ee538931d53a38f68fc0256309e4
daniel8rc <root@localhost.localdomain>
2019-09-16 15:36:45 -0400

    First version of the scanner finished.

--- scanner_ports.py
+++ scanner_ports.py
@@ -4,5 +4,20 @@ import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-host = "192.168.163.137"
+host = "192.168.1.138"
 port = 443
+
+class Scanner:
+    def __init__(self, host):
+        self.host = host
+
+    def specific_port_scanner(self, port):
+        if sock.connect_ex((host, port)):
+            print("Port %d is closed." % (port))
+        else:
+            print("Port %d is open." % (port))
+
+
+if __name__ == "__main__":
+    scan = Scanner(host)
+    scan.specific_port_scanner(port)
